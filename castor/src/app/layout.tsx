da128501c8f5ff9401a7ad0a2bfa870e9fc25339
root <root@localhost>
2026-09-04 18:29:40 +0000

    feat: add clerk authentication provider, clerk themes and setup proxy.ts

--- castor/src/app/layout.tsx
+++ castor/src/app/layout.tsx
@@ -1,7 +1,17 @@
 import type { Metadata } from "next"
-import { IBM_Plex_Mono, Inter } from "next/font/google"
-import "./globals.css"
+import { Darker_Grotesque, IBM_Plex_Mono, Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+
+import "./globals.css"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,19 +35,37 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute={"class"}
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        theme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.variable} ${plexMono.variable} antialiased`}>
+          <ThemeProvider
+            attribute={"class"}
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <header>
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton>
+                  <button className="rounded bg-rose-500 p-2 text-white">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
+
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
