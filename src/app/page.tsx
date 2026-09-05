c35ac44b7d7e82e0303cc51d51d4f2b66cc98a73
root <root@localhost>
2026-09-05 07:27:21 +0000

    feat: implement convex and convex schemas for the oncoming data we need to deal wit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+"use client"
+
+import { useMutation, useQuery } from "convex/react"
+import { api } from "../../convex/_generated/api"
 import { Button } from "@/components/ui/button"
 
 const x = () => {
+  const projects = useQuery(api.projects.get)
+  const createProject = useMutation(api.projects.create)
   return (
-    <div>
-      <Button variant={"destructive"}>Click Me</Button>
+    <div className="flex flex-col gap-2 p-4">
+      <Button
+        onClick={() =>
+          createProject({
+            name: "New Project",
+          })
+        }
+      >
+        Add New
+      </Button>
+
+      {projects?.map((project) => (
+        <div className="flex flex-col rounded border p-2" key={project._id}>
+          <p>{project.name}</p>
+          <p>Owner ID: {project.ownerId}</p>
+        </div>
+      ))}
     </div>
   )
 }
