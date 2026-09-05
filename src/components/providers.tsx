a418b5ed41ff9daf3ce33b2071f327dcb233b0b8
Oliver Metz <softwaredevdad@gmail.com>
2026-09-05 12:03:35 +0200

    feat: db setup and extra auth checks, aswell some auth related views

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -15,6 +15,8 @@ import {
 } from "convex/react"
 import { ConvexProviderWithClerk } from "convex/react-clerk"
 import { ThemeProvider } from "@/components/theme-provider"
+import { UnauthenticatedView } from "@/features/auth/components/unauthenticated-view"
+import { AuthLoadingView } from "@/features/auth/components/auth-loading"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
@@ -33,10 +35,11 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
             {children}
           </Authenticated>
           <Unauthenticated>
-            <SignInButton />
-            <SignUpButton />
+            <UnauthenticatedView />
           </Unauthenticated>
-          <AuthLoading>Authentication Loading...</AuthLoading>
+          <AuthLoading>
+            <AuthLoadingView />
+          </AuthLoading>
         </ThemeProvider>
       </ConvexProviderWithClerk>
     </ClerkProvider>
